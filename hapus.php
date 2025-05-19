15d4472a150c6f14863324fbc09b999a7d395b47
GrandisAzizah <124240045@student.upnyk.ac.id>
2025-05-19 23:20:59 +0700

    tambah session

--- hapus.php
+++ hapus.php
@@ -1,4 +1,12 @@
 <?php
+
+session_start();
+// user belum login
+if (!isset($_SESSION["login"])) {
+    header("location: login.php");
+    exit;
+}
+
 require 'functions.php';
 // ambil id yang dikirim ketika tombol dipencet
 // id dikirim lewat url
