ff53d1813b2db03194a666f4359a3629ffc490fb
GrandisAzizah <124240045@student.upnyk.ac.id>
2025-05-21 23:39:23 +0700

    css ges

--- registrasi.php
+++ registrasi.php
@@ -18,10 +18,14 @@ if (isset($_POST["register"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style_login.css">
     <title>Halaman Registrasi</title>
 </head>
 
-<style>
+<!-- <style>
     label,
     button {
         display: block;
@@ -31,27 +35,51 @@ if (isset($_POST["register"])) {
         padding: 4px;
         margin-top: 10px;
     }
-</style>
+</style> -->
 
 <body>
-    <h1>Halaman Registrasi</h1>
 
-    <form action="" method="POST">
-        <label for="username">Username:</label>
-        <input type="text" name="username" id="username" required>
+    <div class="row no-gutters">
 
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email" required>
+        <!-- bagian kiri -->
+        <div class="col-md-6 no-gutters">
+            <div class="leftside d-flex justify-content-center align-items-center"></div>
+        </div>
 
-        <label for="password">Password:</label>
-        <input type="password" name="password" id="password" required>
+        <!-- bagian kanan -->
+        <div class="col-md-6 no-gutters">
+            <div class="rightside d-flex justify-content-center align-items-center">
+                <form action="" method="POST">
+                    <div>
+                        <h1>Registrasi</h1>
+                    </div>
 
-        <label for="password2">Konformasi Password</label>
-        <input type="password" name="password2" id="password2" required>
+                    <div class="form-group">
+                        <label for="username" class="form-label">Username:</label>
+                        <input type="text" name="username" id="username" class="form-control" minlength="8" maxlength="20" required>
+                    </div>
 
+                    <div class="form-group">
+                        <label for="email" class="form-label">Email:</label>
+                        <input type="email" name="email" id="email" class="form-control" placeholder="example@gmail.com" required>
+                    </div>
 
-        <button type="submit" name="register">Register</button>
-    </form>
+                    <div class="form-group">
+                        <label for="password" class="form-label">Password:</label>
+                        <input type="password" name="password" id="password" class="form-control" minlength="8" maxlength="12" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password2" class="form-label">Konformasi Password</label>
+                        <input type="password" name="password2" id="password2" class="form-control" minlength="8" maxlength="12" required>
+                    </div>
+                    <div>
+                        <p>Sudah punya akun? <a href="login.php">Login di sini</a></p>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" name="register" class="btn btn-primary w-100">Register</button>
+                    </div>
+                </form>
 </body>
 
 </html>
